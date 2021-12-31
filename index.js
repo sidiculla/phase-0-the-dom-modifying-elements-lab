dd98fa16f740381b303d133fc7b2228c04ba326a
sojin <sojinidiculla7@gmail.com>
2021-12-31 15:07:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+var el = document.getElementById('main');
+el.remove();
+
+
+var newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML="sojin is the champion";
